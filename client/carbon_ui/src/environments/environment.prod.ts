6db743aef8e3f1aa14da16d305a65b4724639ad3
Gurvinder Singh <gurvsin3@in.ibm.com>
2020-10-07 14:57:16 +0530

    Changes for Openshift deployment

--- client/carbon_ui/src/environments/environment.prod.ts
+++ client/carbon_ui/src/environments/environment.prod.ts
@@ -19,9 +19,9 @@ export const environment = {
 	isMockEnabled: true, // You have to switch this, when your real back-end is done
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
 	keycloakConfig,
-	ACCOUNTS_API_URL: 'https://accounts.smartthings.com',
-	IOT_API_URL: 'https://iot.smartthings.com',
+	ACCOUNTS_API_URL: 'https://account-svc-smartthings.apps.cairns.os.fyre.ibm.com',
+	IOT_API_URL: 'https://iot-svc-smartthings.apps.cairns.os.fyre.ibm.com',
 	cognos_api_js: 'https://eu-gb.dynamic-dashboard-embedded.cloud.ibm.com/daas/CognosApi.js',
 	cognos_root_url: 'https://eu-gb.dynamic-dashboard-embedded.cloud.ibm.com/daas/',
-	cognos_web_domain: 'https://www.smartthings.com'
+	cognos_web_domain: 'https://ui-svc-smartthings.apps.cairns.os.fyre.ibm.com'
 };
